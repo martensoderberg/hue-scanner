3026bc3e82c85cb5715f40bf7a9bdf603e53c3a3
Marten Soderberg <marten@martensoderberg.se>
2018-03-16 22:58:29 +0000

    improved scan

--- scan.sh
+++ scan.sh
@@ -1,7 +1,9 @@
 arp-scan \
   --interface=wlan0 \
   --retry=2 \
-  --timeout=50 \
+  --timeout=150 \
   --bandwidth=1M \
   --backoff=1 \
+  --quiet \
+  --destaddr=$1 \
   192.168.1.180-192.168.1.199
