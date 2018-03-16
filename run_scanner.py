02aea6b526575b6f8b2cefec48a0cb939f3e2830
Marten Soderberg <marten@martensoderberg.se>
2018-03-16 23:29:15 +0000

    first working scan

--- run_scanner.py
+++ run_scanner.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 
-devices = []
-def read_mac_addresses:
-with open('mac_addresses') as f:
-    content = f.readlines()
-    content = map(lambda s: s.strip(), content)
-    for line in content:
-        if line:
-            fields = line.split(',')
-            mac_address = fields[0]
-            description = fields[1]
-            devices.append((mac_address, description))
+from subprocess import call
+
+def read_mac_addresses():
+    devices = []
+    with open('mac_addresses') as f:
+        content = f.readlines()
+        content = map(lambda s: s.strip(), content)
+        for line in content:
+            if line:
+                fields = line.split(',')
+                mac_address = fields[0]
+                description = fields[1]
+                devices.append((mac_address, description))
+    return devices
+
+def scan_for_devices(devices):
+    for mac_address, description in devices:
+        print("Scanning for " + description + "...")
+        call(["./scan.sh", mac_address])
+
+
+devices = read_mac_addresses()
+scan_for_devices(devices)
 
 
