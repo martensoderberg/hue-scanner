82c1d95ec620afcee05d5ccc5a3cd45b3ae7f44b
Marten Soderberg <marten@martensoderberg.se>
2018-03-17 15:06:10 +0000

    classified Device

--- run_scanner.py
+++ run_scanner.py
@@ -2,8 +2,20 @@
 
 from subprocess import call, check_output
 
+devices = []
+class Device:
+    def __init__(self, mac_address, description):
+        self.mac_address = mac_address
+        self.description = description
+        self.status = "Unknown!"
+
+    def update_presence_status(self, new_status):
+        self.status = new_status
+
+    def __str__(self):
+        return self.description + " -- " + self.status
+
 def read_mac_addresses():
-    devices = []
     with open('mac_addresses') as f:
         content = f.readlines()
         content = map(lambda s: s.strip(), content)
@@ -12,20 +24,29 @@ def read_mac_addresses():
                 fields = line.split(',')
                 mac_address = fields[0]
                 description = fields[1]
-                devices.append((mac_address, description))
-    return devices
+                device = Device(mac_address, description)
+                devices.append(device)
 
 def scan_for_devices(devices):
-    for mac_address, description in devices:
-        print("Scanning for " + description + "...")
-        result = check_output(["./scan.sh", mac_address]).strip()
-        if result == b'1':
-            print("Present and accounted for!")
+    for device in devices:
+        print("Scanning for " + device.description + "...")
+        result = check_output(["./scan.sh", device.mac_address]).strip()
+        if result == b'0':
+            new_status = "Gone!"
+        elif result == b'1':
+            new_status = "Here!"
         else:
-            print("Gone with the wind!")
+            new_status = "Whoops..."
+        device.update_presence_status(new_status)
 
+def turn_lights_off():
+    call(["hue lights off"])
 
-devices = read_mac_addresses()
+def turn_lights_on():
+    call(["hue lights on"])
+
+read_mac_addresses()
 scan_for_devices(devices)
-
+for d in devices:
+    print(d)
 
