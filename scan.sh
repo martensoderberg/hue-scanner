3aebe4666ae26d13218c5e49e9c3d91d237e07ff
Marten Soderberg <marten@martensoderberg.se>
2018-03-17 13:39:15 +0000

    variablized a constant in scan.sh

--- scan.sh
+++ scan.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# ip_range should be your routers DHCP range
+ip_range="192.168.1.180-192.168.1.199"
 arp-scan \
   --interface=wlan0 \
   --retry=2 \
@@ -8,5 +10,5 @@ arp-scan \
   --quiet \
   --numeric \
   --destaddr=$1 \
-  192.168.1.180-192.168.1.199 \
+  $ip_range \
 | grep $1 | uniq | wc -l
